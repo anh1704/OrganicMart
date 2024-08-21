3ebe91228de80fcfc81d475126e448a345741f61
anh1704 <truongngocanh17042004@gmail.com>
2024-08-21 15:47:52 +0700

    spring security

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -5,8 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, World update!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page!";
     }
 }
